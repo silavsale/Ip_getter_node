a93278eb06cf46f5881034e888ea95e2deeb1b03
MoonWalker <silavsale@gmail.com>
2023-08-03 21:06:15 -0400

    add Express

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
 Simple App that creates API to get the IP of the Linux server it running on.
+
+Install pm2 (PM2 is a process manager for the JavaScript runtime Node.js. ) to run your app when 
+
+sudo npm install -g pm2
---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+const port = 3000;
+
+app.get('/api/ip', (req, res) => {
+  const networkInterfaces = os.networkInterfaces();
+  let ip;
+
+  for(let interfaceKey in networkInterfaces) {
+    const networkInterface = networkInterfaces[interfaceKey];
+    
+    const ipv4 = networkInterface.find(net => net.family === 'IPv4' && !net.internal);
+
+    if(ipv4) {
+      ip = ipv4.address;
+      break;
+    }
+  }
+
+  res.send(ip);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
